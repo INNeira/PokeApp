37a625ace5e7805687581e2639ab8c0ce26c60cc
Ignacio Neira <ignacio.neira@incluit.com>
2022-11-02 21:35:14 -0300

    Change: Components -> Pokemons

---
+++ src/pokemons/components/PokemonCard/PokemonCardData.jsx
@@ -0,0 +1,61 @@
+import React, { useEffect, useState } from "react";
+import {
+  Box,
+  UnorderedList,
+  ListItem,
+  Text,
+  Image,
+  Heading,
+} from "@chakra-ui/react";
+import { pokemonService } from "../../../shared/services/pokemonService";
+import { firstLetterToUpperCase } from "../../../shared/helpers/string-helper";
+
+export const PokemonCardData = ({ url, name }) => {
+  const [pokemonsData, setPokemonsData] = useState([]);
+
+  const getPokemonData = async () => {
+    const pokemonsData = await pokemonService.getPokemonByURL(url);
+    setPokemonsData(pokemonsData);
+  };
+
+  useEffect(() => {
+    getPokemonData();
+  }, []);
+
+  return (
+    <Box bgGradient='linear(to-b, yellow.200, yellow.400)' borderRadius={12}>
+      {pokemonsData.map((pokeData) => (
+        <Box key={pokeData.id}>
+          <Box
+            className="imgDiv"
+            display={"flex"}
+            justifyContent={"center"}
+            alignItems={"center"}
+          >
+            <Image
+              boxSize={"3xs"}
+              objectFit={"cover"}
+              src={pokeData.sprites.front_default}
+              m={0}
+            ></Image>
+          </Box>
+
+          <Box className="pokeInfo" p={5} minH={44}>
+            <Heading fontSize="xl">{firstLetterToUpperCase(name)}</Heading>
+            <Text as={"sub"}>N°: {pokeData.order}</Text>
+            <UnorderedList marginInlineStart={0} mt={3}>
+              <Text fontWeight={"semibold"} as={"i"}>
+                Types
+              </Text>
+              {pokeData.types.map((el) => (
+                <ListItem listStyleType={"none"} key={pokeData.order + el.slot}>
+                  {firstLetterToUpperCase(el.type.name)}
+                </ListItem>
+              ))}
+            </UnorderedList>
+          </Box>
+        </Box>
+      ))}
+    </Box>
+  );
+};
--- src/pokemons/components/PokemonGrid.jsx
+++ src/pokemons/components/PokemonGrid.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
-import { Box, Flex, HStack } from "@chakra-ui/react";
+import { Box, Flex, Heading, Text } from "@chakra-ui/react";
 import { pokemonService } from "../../shared/services/pokemonService";
-import { PokemonCard } from "./PokemonCard";
+import { PokemonCard } from "./PokemonCard/PokemonCard";
 
 export const PokemonGrid = () => {
   const [pokemons, setPokemons] = useState([]);
@@ -16,8 +16,37 @@ export const PokemonGrid = () => {
   }, []);
 
   return (
-    <Box m={8}>
-      <Flex className="cardsFather" flexWrap={"wrap"} justifyContent={"center"} alignItems={"center"}>
+    <Box className="bigFather" bg={'whiteAlpha.800'}>
+      <Box
+        display={"block"}
+        textAlign={'center'}
+        width={"80%"}
+        margin={"auto"}
+        padding={40}
+        pb={24}
+      >
+        <Text
+          bgClip="text"
+          fontSize="8xl"
+          fontWeight="extrabold"
+          bgGradient="linear(to-l, yellow.400, yellow.600)"
+          mb={8}
+        >
+          Welcome to PokeApp
+        </Text>
+        <Text fontSize="3xl" fontWeight={'semibold'}>
+            Here is a list of some of the <br />
+            the pokemons you can view.
+        </Text>
+      </Box>
+      <Flex
+        className="cardsFather"
+        flexWrap={"wrap"}
+        justifyContent={"center"}
+        alignItems={"center"}
+        width={"80%"}
+        margin={"auto"}
+      >
         {pokemons.map((pokemon) => (
           <PokemonCard key={pokemon.name} {...pokemon} />
         ))}
