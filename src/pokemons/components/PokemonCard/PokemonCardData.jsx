286aea9481760b33949ba48da25a6960f69efdad
Ignacio Neira <ignacio.neira@incluit.com>
2022-11-04 16:35:40 -0300

    Update : PokemonsCard Component

--- src/pokemons/components/PokemonCard/PokemonCardData.jsx
+++ src/pokemons/components/PokemonCard/PokemonCardData.jsx
@@ -10,11 +10,13 @@ import {
 import { pokemonService } from "../../../shared/services/pokemonService";
 import { firstLetterToUpperCase } from "../../../shared/helpers/string-helper";
 
+
+
 export const PokemonCardData = ({ url, name }) => {
   const [pokemonsData, setPokemonsData] = useState([]);
 
   const getPokemonData = async () => {
-    const pokemonsData = await pokemonService.getPokemonByURL(url);
+    const pokemonsData = await pokemonService.getPokemonByName(name);
     setPokemonsData(pokemonsData);
   };
 
@@ -22,40 +24,47 @@ export const PokemonCardData = ({ url, name }) => {
     getPokemonData();
   }, []);
 
-  return (
-    <Box bgGradient='linear(to-b, yellow.200, yellow.400)' borderRadius={12}>
-      {pokemonsData.map((pokeData) => (
-        <Box key={pokeData.id}>
-          <Box
-            className="imgDiv"
-            display={"flex"}
-            justifyContent={"center"}
-            alignItems={"center"}
-          >
-            <Image
-              boxSize={"3xs"}
-              objectFit={"cover"}
-              src={pokeData.sprites.front_default}
-              m={0}
-            ></Image>
-          </Box>
+  // getPokemonData();
 
-          <Box className="pokeInfo" p={5} minH={44}>
-            <Heading fontSize="xl">{firstLetterToUpperCase(name)}</Heading>
-            <Text as={"sub"}>N°: {pokeData.order}</Text>
-            <UnorderedList marginInlineStart={0} mt={3}>
-              <Text fontWeight={"semibold"} as={"i"}>
-                Types
-              </Text>
-              {pokeData.types.map((el) => (
-                <ListItem listStyleType={"none"} key={pokeData.order + el.slot}>
-                  {firstLetterToUpperCase(el.type.name)}
-                </ListItem>
-              ))}
-            </UnorderedList>
+  return (
+    <Box className="pokeContainer" w={"2xs"} m={3} shadow="sm" borderWidth={0}>
+      <Box bgGradient="linear(to-b, yellow.200, yellow.400)" borderRadius={12}>
+        {pokemonsData.map((pokeData) => (
+          <Box key={pokeData.id}>
+            <Box
+              className="imgDiv"
+              display={"flex"}
+              justifyContent={"center"}
+              alignItems={"center"}
+            >
+              <Image
+                boxSize={"3xs"}
+                objectFit={"cover"}
+                src={pokeData.sprites.front_default}
+                m={0}
+              ></Image>
+            </Box>
+
+            <Box className="pokeInfo" p={5} minH={44}>
+              <Heading fontSize="xl">{firstLetterToUpperCase(name)}</Heading>
+              <Text as={"sub"}>N°: {pokeData.order}</Text>
+              <UnorderedList marginInlineStart={0} mt={3}>
+                <Text fontWeight={"semibold"} as={"i"}>
+                  Types
+                </Text>
+                {pokeData.types.map((el) => (
+                  <ListItem
+                    listStyleType={"none"}
+                    key={pokeData.order + el.slot}
+                  >
+                    {firstLetterToUpperCase(el.type.name)}
+                  </ListItem>
+                ))}
+              </UnorderedList>
+            </Box>
           </Box>
-        </Box>
-      ))}
+        ))}
+      </Box>
     </Box>
   );
 };
